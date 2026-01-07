d4d8325a2421a09b54a330dc60bd94ce1c9cdf82
StanleyUgoOptisource <stanley.ugo@optisourcetech.com>
2026-01-07 01:14:48 +0100

    finishing up the HomePage and adding cors middleware to backend

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 
 import noteRoutes from './noteRoutes.js';
 import { connectDB } from '../config/db.js';
@@ -10,8 +11,10 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5001;
 
+app.use(cors({
+    origin: '*',
+}));
 app.use(express.json());
-
 //app.use(rateLimiter);
 
 app.use('/api/notes', noteRoutes);
