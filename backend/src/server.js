a6a0e68c43cd7c91078c6a4fa62a987ab542501e
StanleyUgoOptisource <stanley.ugo@optisourcetech.com>
2026-01-09 23:18:01 +0100

    adding production build configurations

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import path from 'path';
 
 import noteRoutes from './noteRoutes.js';
 import { connectDB } from '../config/db.js';
@@ -10,14 +11,30 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
-app.use(cors({
-    origin: '*',
-}));
-app.use(express.json());
+//NOTE: We don't need this cors config if both backend and frontend is served on the same domain
+if (process.env.NODE_ENV !== "production") {
+  app.use(
+    cors({
+      origin: "http://localhost:5173",
+    })
+  );
+}
 //app.use(rateLimiter);
 
 app.use('/api/notes', noteRoutes);
+
+//NOTE: only when the backend and frontend is served on the same domain -  we don't need cors config, just this.
+if (process.env.NODE_ENV === "production")
+{
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+    app.get("*", (req, res) =>
+    {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    });
+}
 
 connectDB().then(() =>
 {
