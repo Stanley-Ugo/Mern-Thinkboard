20c63315a2c5e7c75107cbd48ef69bcd26e445ef
StanleyUgoOptisource <stanley.ugo@optisourcetech.com>
2026-01-09 22:48:58 +0100

    adding details and delete page

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,71 +1,60 @@
-import React from 'react'
-import { useState, useEffect } from 'react'
-import axios from 'axios'
-import toast from 'react-hot-toast'
+import { useState } from "react";
+import Navbar from "../components/Navbar";
+import RateLimitedUI from "../components/RateLimitedUI";
+import { useEffect } from "react";
+import api from "../lib/axios";
+import toast from "react-hot-toast";
+import NoteCard from "../components/NoteCard";
+import NotesNotFound from "../components/NotesNotFound";
 
-import Navbar from '../components/Navbar'
-import RateLimitedUI from '../components/RateLimitedUI'
-import NoteCard from '../components/NoteCard'
+const HomePage = () => {
+  const [isRateLimited, setIsRateLimited] = useState(false);
+  const [notes, setNotes] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-const HomePage = () =>
-{
-    const [isRateLimited, setisRateLimited] = useState(false);
-    const [notes, setNotes] = useState([]);
-    const [loading, setLoading] = useState(false);
-
-    useEffect(() =>
-    {
-        const fetchNotes = async () =>
-        {
-            try
-            {
-                const res = await axios.get('http://localhost:5001/api/notes');
-                console.log(res.data);
-                setNotes(res.data);
-                setisRateLimited(false);
-            } catch (error)
-            {
-                console.error('Error fetching notes:', error);
-                if (error.response && error.response.status === 429)
-                {
-                    setisRateLimited(true);
-                    toast.error('You have been rate limited. Please try again later.');
-                }
-                else
-                {
-                    toast.error('failed to load notes. Please try again later.');
-                }
-            }
-            finally
-            {
-                setLoading(false);
-            }
+  useEffect(() => {
+    const fetchNotes = async () => {
+      try {
+        const res = await api.get("/notes");
+        console.log(res.data);
+        setNotes(res.data);
+        setIsRateLimited(false);
+      } catch (error) {
+        console.log("Error fetching notes");
+        console.log(error.response);
+        if (error.response?.status === 429) {
+          setIsRateLimited(true);
+        } else {
+          toast.error("Failed to load notes");
         }
+      } finally {
+        setLoading(false);
+      }
+    };
 
-        fetchNotes();
-    }, []);
-    return (
-        <div className="min-h-screen">
-            <Navbar />
+    fetchNotes();
+  }, []);
 
-            {isRateLimited && <RateLimitedUI />}
+  return (
+    <div className="min-h-screen">
+      <Navbar />
 
-            <div className="max-w-7xl mx-auto p-4 mt-6">
-                {loading && <div className="text-center text-primary py-1">Loading Notes...</div>}
+      {isRateLimited && <RateLimitedUI />}
 
-                {notes.length > 0 && !isRateLimited && (
-                    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-                        {
-                            notes.map(note =>
-                            {
-                                <NoteCard key={note._id} note={note} />
-                            })
-                        }
-                    </div>
-                )}
-            </div>
-        </div >
-    )
-}
+      <div className="max-w-7xl mx-auto p-4 mt-6">
+        {loading && <div className="text-center text-primary py-10">Loading notes...</div>}
 
-export default HomePage
+        {notes.length === 0 && !isRateLimited && <NotesNotFound />}
+
+        {notes.length > 0 && !isRateLimited && (
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+            {notes.map((note) => (
+              <NoteCard key={note._id} note={note} setNotes={setNotes} />
+            ))}
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+export default HomePage;
